4426dcca75edc9d29234571978141e3048a94e94
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-04-25 09:06:24 -0600

    Repository Created

---
+++ src/proyecto/git/prueba/pkg1/ProyectoGitPrueba1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.git.prueba.pkg1;
+
+/**
+ *
+ * @author Samuel Vasquez
+ */
+public class ProyectoGitPrueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println("REPO 1");
+        // TODO code application logic here
+    }
+    
+}
